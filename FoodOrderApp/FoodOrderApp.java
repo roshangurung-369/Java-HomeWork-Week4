06894490026baed50810ce6675cc51e04959f961
roshangurung-369 <roshan.gurung2025@mic.edu.np>
2026-06-19 22:26:30 +0545

    Added Food Order app folder

---
+++ FoodOrderApp/FoodOrderApp.java
@@ -0,0 +1,22 @@
+class FoodItem {
+    private String name;
+    private double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayMenuItem() {
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
