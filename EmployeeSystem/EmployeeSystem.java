4ff7549026f59405629ec59f98862926fb7d6609
roshangurung-369 <roshan.gurung2025@mic.edu.np>
2026-06-19 21:50:57 +0545

    added the Employee System Folder

---
+++ EmployeeSystem/EmployeeSystem.java
@@ -0,0 +1,27 @@
+class Employee {
+    private String name;
+    private int id;
+    private double baseSalary;
+
+    public Employee(String name, int id, double baseSalary) {
+        this.name = name;
+        this.id = id;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public double calculateSalary() {
+        return baseSalary;
+    }
+}
