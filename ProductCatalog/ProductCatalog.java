ab2ff6b5f5f1c9c3b51dc28bca7e766f09395d58
roshangurung-369 <roshan.gurung2025@mic.edu.np>
2026-06-19 21:59:21 +0545

    added the Product Catalog Folder

---
+++ ProductCatalog/ProductCatalog.java
@@ -0,0 +1,22 @@
+class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Product Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
